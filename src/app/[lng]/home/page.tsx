847226f3568edf5dc9fdfb4094436d340a47710d
Brahian Giraldo <brahian.giraldo797@pascualbravo.edu.co>
2024-11-07 17:52:29 -0500

    redirect dashboard to page

--- src/app/[lng]/home/page.tsx
+++ src/app/[lng]/home/page.tsx
@@ -1,21 +1,27 @@
-'use client'
-import { useEffect, useState } from 'react'
+"use client"
+import { useEffect, useState } from "react"
 
 // images
 
 // components, interfaces, types and functions
 
-import { SearchParams } from '@/models/interfaces/ParamsProps'
-import { changeLanguage, getLang, InitializeLanguage, validateLangUrl } from '@/helpers/language'
-import { onActiveLinks, onDetectRedirect } from '@/helpers/redirectCustom'
-import getLanguage, { LanguagesType } from '@/models/i18n'
-import { Seo } from '@/components/atoms/seo/Seo'
-import Navbar from '@/components/organism/navbar/Navbar'
-import ContactUs from '@/components/organism/contactUs/ContactUs'
-import Footer from '@/components/organism/footer/Footer'
+import { SearchParams } from "@/models/interfaces/ParamsProps"
+import {
+  changeLanguage,
+  getLang,
+  InitializeLanguage,
+  validateLangUrl,
+} from "@/helpers/language"
+import { onActiveLinks, onDetectRedirect } from "@/helpers/redirectCustom"
+import getLanguage, { LanguagesType } from "@/models/i18n"
+import { Seo } from "@/components/atoms/seo/Seo"
+import Navbar from "@/components/organism/navbar/Navbar"
+import ContactUs from "@/components/organism/contactUs/ContactUs"
+import Footer from "@/components/organism/footer/Footer"
+import DashBoard from "@/components/organism/dashBoard/DashBoard"
+import { IconFactory } from "@/components/atoms/icons/domain/use-cases/factoryIcon"
 
 const HomePage = ({ params: { lng } }: SearchParams) => {
-
   const [lang, setLang] = useState<LanguagesType>(lng)
   const defaultLanguage = getLang()
 
@@ -29,21 +35,87 @@ const HomePage = ({ params: { lng } }: SearchParams) => {
   useEffect(() => {
     onActiveLinks()
     onDetectRedirect()
-  },[])
+  }, [])
 
-  
+  const cards = [
+    {
+      title: "5",
+      subtitle: "Activo",
+      showWave: true,
+      widget: (
+        <IconFactory color="#AABDFF" height={60} width={64} name="HomeSimple" />
+      ),
+      percentageWidth: 100,
+      percentageHeight: 80,
+    },
+    {
+      title: "36",
+      subtitle: "Temperatura Ambiental",
+      showWave: true,
+      widget: (
+        <IconFactory color="#AABDFF" height={60} width={64} name="HomeSimple" />
+      ),
+      percentageWidth: 100,
+      percentageHeight: 80,
+    },
+    {
+      title: "80",
+      subtitle: "Humedad Ambiental",
+      showWave: true,
+      widget: (
+        <IconFactory color="#AABDFF" height={60} width={64} name="HomeSimple" />
+      ),
+      percentageWidth: 100,
+      percentageHeight: 80,
+    },
+    {
+      title: "80",
+      subtitle: "Temperatura Corporal",
+      showWave: true,
+      widget: (
+        <IconFactory color="#AABDFF" height={60} width={64} name="HomeSimple" />
+      ),
+      percentageWidth: 100,
+      percentageHeight: 80,
+    },
+    {
+      title: "100",
+      subtitle: "Hist. Temp Corporal",
+      showWave: true,
+      widget: (
+        <IconFactory color="#AABDFF" height={60} width={64} name="HomeSimple" />
+      ),
+      percentageWidth: 100,
+      percentageHeight: 80,
+    },
+    {
+      title: "70",
+      subtitle: "Saturacion",
+      showWave: true,
+      widget: (
+        <IconFactory color="#AABDFF" height={60} width={64} name="HomeSimple" />
+      ),
+      percentageWidth: 100,
+      percentageHeight: 80,
+    },
+  ]
+
   return (
     <>
-      <Seo
+      <DashBoard cards={cards} />
+    </>
+  )
+}
+
+export default HomePage
+
+{
+  /* <Seo
         title={translate.HOME.TITLE}
         description={translate.HOME.DESCRIPTION}
         lang={lang}
       />
      <Navbar changeLang={changeLang} lng={lang} defaultLang={defaultLanguage} />
       <ContactUs lng={lang} defaultLang={defaultLanguage}/>
-      <Footer lng={lang} defaultLang={defaultLanguage} />
-    </>
-  )
+      <Footer lng={lang} defaultLang={defaultLanguage} /> */
 }
-
-export default HomePage
